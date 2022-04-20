4641770d8c2eec354afa3311fd6c466628b2077a
Ismael Cedano <iocedano@gmail.com>
2022-04-20 14:09:31 -0400

    init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['public/*', 'node_modules/*', 'dist/*']
+};
